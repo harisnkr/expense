b45ec4269728220c3934596d1da041d65307c53f
Hari <h4ri@icloud.com>
2024-04-06 15:19:31 +0800

    structs for Models

--- models/moneymgmt.go
+++ models/moneymgmt.go
@@ -1,13 +1,41 @@
 package models
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Budget struct {
-	// TODO
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	UserID    primitive.ObjectID `bson:"user_id"`
+	Category  string             `bson:"category"`
+	Amount    float64            `bson:"amount"`
+	StartDate time.Time          `bson:"start_date"`
+	EndDate   time.Time          `bson:"end_date"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
 }
 
 type Transaction struct {
-	// TODO
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	UserID      primitive.ObjectID `bson:"user_id"`
+	Type        string             `bson:"type"`
+	Category    string             `bson:"category"`
+	Amount      float64            `bson:"amount"`
+	Date        time.Time          `bson:"date"`
+	Description string             `bson:"description"`
+	CreatedAt   time.Time          `bson:"created_at"`
+	UpdatedAt   time.Time          `bson:"updated_at"`
 }
 
 type Savings struct {
-	// TODO
+	ID            primitive.ObjectID `bson:"_id,omitempty"`
+	UserID        primitive.ObjectID `bson:"user_id"`
+	Description   string             `bson:"description"`
+	TargetAmount  float64            `bson:"target_amount"`
+	CurrentAmount float64            `bson:"current_amount"`
+	Deadline      time.Time          `bson:"deadline"`
+	CreatedAt     time.Time          `bson:"created_at"`
+	UpdatedAt     time.Time          `bson:"updated_at"`
 }
